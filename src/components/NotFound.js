4dd9945cc274cd1d070a7faf2302cb0c995fa3ca
Dany <Dany@JB.prj>
2023-08-03 19:35:09 +0300

    fix 1

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div style={{textAlign: 'center'}}>
+        <h1>404 Error</h1>
+        <p>{item.error}</p>
+        </div>
+  )
+}
